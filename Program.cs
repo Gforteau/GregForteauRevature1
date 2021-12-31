4dd04bedab43c63f5f0e4f2d4fcfb285ceb8e2bc
Gforteau <95656795+Gforteau@users.noreply.github.com>
2021-12-31 17:08:27 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,139 +1,120 @@
 ﻿using System;
 
-namespace StringManipulationChallenge
+namespace _4_MethodsChallenge
 {
     public class Program
     {
-        static void Main(string[] args)
+        private static string name;
+
+        public static void Main(string[] args)
         {
-            //StringToUpper
-            string toUpper;
-            Console.Write("Enter a lowercase string: ");
-            toUpper = StringToUpper(Console.ReadLine());
-            Console.WriteLine($"New string is: {toUpper}\n");
+            string name, greeting;
+            name = GetName();
+            greeting = GreetFriend(name);
+            Console.WriteLine(greeting + "\n");
 
-            //StringToLower
-            string toLower;
-            Console.Write("Enter an uppercase string: ");
-            toLower = StringToLower(Console.ReadLine());
-            Console.WriteLine($"New string is: {toLower}\n");
+            double x, y, result;
+            x = GetNumber();
+            y = GetNumber();
 
-            //StringTrim
-            string trim;
-            Console.Write("Enter string with whitespace before and after: ");
-            trim = StringTrim(Console.ReadLine());
-            Console.WriteLine($"New string is: {trim}\n");
-
-            //StringSubstring
-            string testString = "This is a string!";
-            Console.WriteLine(testString);
-            Console.Write("Enter an start index to get substring from string above: ");
-            int index = Int32.Parse(Console.ReadLine());
-            Console.Write("Enter the length of the substring: ");
-            int length = Int32.Parse(Console.ReadLine());
-            string subString = StringSubstring(testString, index, length);
-            Console.WriteLine($"Substring: {subString}\n");
-
-            //SearchChar
-            string input;
-            char a;
-            Console.Write("Enter a string: ");
-            input = Console.ReadLine();
-            Console.Write("Enter a char you want to search for: ");
-            a = char.Parse(Console.ReadLine());
-            int charIndex = SearchChar(input, a);
-            Console.WriteLine($"Index of char {a} is {charIndex}\n");
-
-            //ConcatNames
-            string fName, lName, fullName;
-            Console.Write("Enter first name: ");
-            fName = Console.ReadLine();
-            Console.Write("Enter last name: ");
-            lName = Console.ReadLine();
-            fullName = ConcatNames(fName, lName);
-            Console.WriteLine($"Hello {fullName}!");
+            int action;
+            action = GetAction();
+            result = DoAction(x, y, action);
         }
 
-        /// <summary>
-        /// This method has one string parameter and will: 
-        /// 1) change the string to all upper case and 
-        /// 2) return the new string.
-        /// </summary>
-        /// <param name="usersString"></param>
-        /// <returns></returns>
-        public static string StringToUpper(string usersString)
+        private static string GetName()
         {
-            return usersString.ToUpper();
+            return name;
+                }
+
+        public static string GetName(string s1)
+        {
+            string name;
+            Console.Write("Please enter your name: ");
+            name = Console.ReadLine();
+            return name;
         }
 
-        /// <summary>
-        /// This method has one string parameter and will:
-        /// 1) change the string to all lower case,
-        /// 2) return the new string into the 'lowerCaseString' variable
-        /// </summary>
-        /// <param name="usersString"></param>
-        /// <returns></returns>       
-        public static string StringToLower(string usersString)
+        public static string GreetFriend(string name)
         {
-            return usersString.ToLower();
+            return $"Hello, {name}. You are my friend.";
         }
 
-        /// <summary>
-        /// This method has one string parameter and will:
-        /// 1) trim the whitespace from before and after the string, and
-        /// 2) return the new string.
-        /// HINT: When getting input from the user (you are the user), try inputting "   a string with whitespace   " to see how the whitespace is trimmed.
-        /// </summary>
-        /// <param name="usersStringWithWhiteSpace"></param>
-        /// <returns></returns>
-        public static string StringTrim(string usersStringWithWhiteSpace)
+        public static double GetNumber()
         {
-            return usersStringWithWhiteSpace.Trim();
+            double convertedNum;
+            bool isNum;
+            do
+            {
+                Console.Write("Enter a double: ");
+                isNum = Double.TryParse(Console.ReadLine(), out convertedNum);
+                if (!isNum)
+                    Console.WriteLine("Invalid input!");
+            } while (!isNum);
+            return convertedNum;
         }
 
-        /// <summary>
-        /// This method has three parameters, one string and two integers. It will:
-        /// 1) get the substring based on the first integer element and the length 
-        /// of the substring desired.
-        /// 2) return the substring.
-        /// </summary>
-        /// <param name="x"></param>
-        /// <param name="firstElement"></param>
-        /// <param name="lastElement"></param>
-        /// <returns></returns>
-        public static string StringSubstring(string x, int firstElement, int lengthOfSubsring)
+        public static int GetAction()
         {
-            return x.Substring(firstElement, lengthOfSubsring);
+            bool validInput = false;
+            int choice = 0;
+            do
+            {
+                Console.Write("Enter 1)Add, 2)Subtract, 3)Multiply, or 4)Divide: ");
+                string input = Console.ReadLine();
+                switch (input)
+                {
+                    case "1":
+                        choice = 1;
+                        validInput = true;
+                        break;
+                    case "2":
+                        choice = 2;
+                        validInput = true;
+                        break;
+                    case "3":
+                        choice = 3;
+                        validInput = true;
+                        break;
+                    case "4":
+                        choice = 4;
+                        validInput = true;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input!");
+                        break;
+                }
+            } while (!validInput);
+            return choice;
         }
 
-        /// <summary>
-        /// This method has two parameters, one string and one char. It will:
-        /// 1) search the string parameter for first occurrance of the char parameter and
-        /// 2) return the index of the char.
-        /// HINT: Think about how StringTrim() (above) would be useful in this situation
-        /// when getting the char from the user. 
-        /// </summary>
-        /// <param name="userInputString"></param>
-        /// <param name="charUserWants"></param>
-        /// <returns></returns>
-        public static int SearchChar(string userInputString, char charUserWants)
+        public static double DoAction(double x, double y, int action)
         {
-            return userInputString.IndexOf(charUserWants);
-        }
+            double result;
 
-        /// <summary>
-        /// This method has two string parameters. It will:
-        /// 1) concatenate the two strings with a space between them.
-        /// 2) return the new string.
-        /// HINT: You will need to get the users first and last name in the 
-        /// main method and send them as arguments.
-        /// </summary>
-        /// <param name="fName"></param>
-        /// <param name="lName"></param>
-        /// <returns></returns>
-        public static string ConcatNames(string fName, string lName)
-        {
-            return $"{fName} {lName}";
+            switch (action)
+            {
+                case 1:
+                    result = x + y;
+                    Console.WriteLine($"{x} + {y} = {result}");
+                    break;
+                case 2:
+                    result = x - y;
+                    Console.WriteLine($"{x} - {y} = {result}");
+                    break;
+                case 3:
+                    result = x * y;
+                    Console.WriteLine($"{x} * {y} = {result}");
+                    break;
+                case 4:
+                    result = x / y;
+                    Console.WriteLine($"{x} / {y} = {result}");
+                    break;
+                default:
+                    result = 0;
+                    throw new FormatException();
+            }
+            return Math.Abs(result);
         }
-    }//end of program
+    }
 }
