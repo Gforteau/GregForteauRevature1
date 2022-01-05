88a0ea53b8a0cb0ffe8e9bda3c6ad42c55128bd4
wangyu2579 <greg.forteau@revature.net>
2022-01-04 21:30:39 -0500

    sweetNsalty

---
+++ Challenges/SweetNSalty/SweetNSalty/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace SweetNSalty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter(); // Method to count sweet, salty, sweetNsalty
+
+        }
+        public static void Counter()
+        {
+            ArrayList Alist = new ArrayList();
+            int sCount = 0;
+            int slCount = 0;
+            int sNsCount = 0;
+
+            for (int i = 1; i < 1000; i++)
+            {
+
+                if (i % 20 == 0)     //groups of twenty
+                {
+                    Console.WriteLine();
+                }
+
+                if (i % 5 == 0 && i % 3 == 0 && i > 1)
+                {
+                    Console.Write("Sweet n Salty, ");
+                    sNsCount++;
+                }
+                else if (i % 3 == 0 && i > 1)
+                {
+                    Console.Write("Sweet, ");
+                    sCount++;
+                }
+                else if (i % 5 == 0 && i > 1)
+                {
+                    Console.Write("Salty, ");
+                    slCount++;
+                }
+                else
+                {
+                    Console.Write($"{ i}, ");
+                }
+
+                if (i == 999)
+                {
+                    Console.WriteLine($"\n\n\n\nSweet appears {sCount} times.");
+                    Console.WriteLine($"Salty appears {slCount} times");
+                    Console.WriteLine($"Sweet N Salty appears {sNsCount} times");
+                }
+
+            }
+
+        }
+    }
+}
+        
+
+
+
+
